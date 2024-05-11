e9ad85895bb0f204b87289f1b7e740741e735bb6
ronjohnson owuor <ronjohnsonowuor83@gmail.com>
2024-05-11 20:07:48 +0300

    added google profiles  to load

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,13 @@ const nextConfig = {
 			{
 				protocol: 'https',
 				hostname: 'machizi.s3.amazonaws.com',
-			  }],
+			  },
+			  {
+				protocol: 'https',
+				hostname: ' lh3.googleusercontent.com',
+			  }		  
+			],
+			
 	  },
 	  typescript:{
 		ignoreBuildErrors:true
