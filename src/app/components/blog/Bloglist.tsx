1908a9d6ff115249d5042f372580d3eaa60a1f1b
ronjohnson owuor <ronjohnsonowuor83@gmail.com>
2024-05-19 09:46:40 +0300

    started working on the blog list

---
+++ src/app/components/blog/Bloglist.tsx
@@ -0,0 +1,95 @@
+import Image from 'next/image'
+import React from 'react'
+import { RiChat1Line } from 'react-icons/ri'
+
+function Bloglist() {
+  return (
+    <div className=' w-full min-h-screen mx-2 my-10'>
+        <h3 className='text-xl md:text-2xl my-4 dark:text-gray mx-4'>Latest new,tips and updates in education sector</h3>
+        <div className='w-[95%] min-h-[100px] grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3  my-10  mx-4 gap-4'>
+
+            <div className='w-full min-h-[100px] flex flex-wrap flex-col p-4 shadow-md  rounded-md'>
+                <img src="https://picsum.photos/300/200" className='w-full h-[300px] object-cover rounded-md mb-2' alt="blog list image" />
+                <h3 className='text-md my-2 w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cum.</h3>
+                <div className='flex justify-start sm:justify-around my-4 flex-wrap gap-4 items-center' id="statistics">
+                    <button className=' w-[100px] h-[40px] bg-lSecondary dark:bg-dSecondary text-gray rounded-md outline-none border-none dark:text-dText  hover:bg-primary'>read</button>
+                    <div className='flex items-center justify-center gap-2'>
+                        <span className='flex items-center gap-4'>👍 &nbsp; 200K </span>
+                        <span className='flex items-center gap-4'><RiChat1Line/> &nbsp; 20K </span>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='w-full min-h-[100px] flex flex-wrap flex-col p-4 shadow-md  rounded-md'>
+                <img src="https://picsum.photos/300/200" className='w-full h-[300px] object-cover rounded-md mb-2' alt="blog list image" />
+                <h3 className='text-md my-2 w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cum.</h3>
+                <div className='flex justify-start sm:justify-around my-4 flex-wrap gap-4 items-center' id="statistics">
+                    <button className=' w-[100px] h-[40px] bg-lSecondary dark:bg-dSecondary text-gray rounded-md outline-none border-none dark:text-dText  hover:bg-primary'>read</button>
+                    <div className='flex items-center justify-center gap-2'>
+                        <span className='flex items-center gap-4'>👍 &nbsp; 200K </span>
+                        <span className='flex items-center gap-4'><RiChat1Line/> &nbsp; 20K </span>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className='w-full min-h-[100px] flex flex-wrap flex-col p-4 shadow-md  rounded-md'>
+                <img src="https://picsum.photos/300/200" className='w-full h-[300px] object-cover rounded-md mb-2' alt="blog list image" />
+                <h3 className='text-md my-2 w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cum.</h3>
+                <div className='flex justify-start sm:justify-around my-4 flex-wrap gap-4 items-center' id="statistics">
+                    <button className=' w-[100px] h-[40px] bg-lSecondary dark:bg-dSecondary text-gray rounded-md outline-none border-none dark:text-dText  hover:bg-primary'>read</button>
+                    <div className='flex items-center justify-center gap-2'>
+                        <span className='flex items-center gap-4'>👍 &nbsp; 200K </span>
+                        <span className='flex items-center gap-4'><RiChat1Line/> &nbsp; 20K </span>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='w-full min-h-[100px] flex flex-wrap flex-col p-4 shadow-md  rounded-md'>
+                <img src="https://picsum.photos/300/200" className='w-full h-[300px] object-cover rounded-md mb-2' alt="blog list image" />
+                <h3 className='text-md my-2 w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cum.</h3>
+                <div className='flex justify-start sm:justify-around my-4 flex-wrap gap-4 items-center' id="statistics">
+                    <button className=' w-[100px] h-[40px] bg-lSecondary dark:bg-dSecondary text-gray rounded-md outline-none border-none dark:text-dText  hover:bg-primary'>read</button>
+                    <div className='flex items-center justify-center gap-2'>
+                        <span className='flex items-center gap-4'>👍 &nbsp; 200K </span>
+                        <span className='flex items-center gap-4'><RiChat1Line/> &nbsp; 20K </span>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='w-full min-h-[100px] flex flex-wrap flex-col p-4 shadow-md  rounded-md'>
+                <img src="https://picsum.photos/300/200" className='w-full h-[300px] object-cover rounded-md mb-2' alt="blog list image" />
+                <h3 className='text-md my-2 w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cum.</h3>
+                <div className='flex justify-start sm:justify-around my-4 flex-wrap gap-4 items-center' id="statistics">
+                    <button className=' w-[100px] h-[40px] bg-lSecondary dark:bg-dSecondary text-gray rounded-md outline-none border-none dark:text-dText  hover:bg-primary'>read</button>
+                    <div className='flex items-center justify-center gap-2'>
+                        <span className='flex items-center gap-4'>👍 &nbsp; 200K </span>
+                        <span className='flex items-center gap-4'><RiChat1Line/> &nbsp; 20K </span>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='w-full min-h-[100px] flex flex-wrap flex-col p-4 shadow-md  rounded-md'>
+                <img src="https://picsum.photos/300/200" className='w-full h-[300px] object-cover rounded-md mb-2' alt="blog list image" />
+                <h3 className='text-md my-2 w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cum.</h3>
+                <div className='flex justify-start sm:justify-around my-4 flex-wrap gap-4 items-center' id="statistics">
+                    <button className=' w-[100px] h-[40px] bg-lSecondary dark:bg-dSecondary text-gray rounded-md outline-none border-none dark:text-dText  hover:bg-primary'>read</button>
+                    <div className='flex items-center justify-center gap-2'>
+                        <span className='flex items-center gap-4'>👍 &nbsp; 200K </span>
+                        <span className='flex items-center gap-4'><RiChat1Line/> &nbsp; 20K </span>
+                    </div>
+                </div>
+            </div>
+            
+
+        </div>
+    </div>
+  )
+}
+
+export default Bloglist
