dcd0e34caf094bd6ebab8b729ce9ba4d31f0306d
ronjohnson-owuor <ronjohnsonowuor83@gmail.com>
2024-06-13 18:01:37 +0300

    started working on new landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-import { Toaster } from "sonner";
-import Footer from "./components/footer/Footer";
-import Prompt from "./components/home/Prompt";
-import Navigation from "./components/navigation/Navigation";
 
-const Page = () => {
-   return (
-     <div id="div_scroll" className="w-full min-h-screen bg-lBackground dark:text-dText dark:bg-dBackground overflow-x-clip">
-      <Toaster/>
-      <Navigation/>
-      <Prompt/>
-      <Footer/>
-    </div> 
-    );
-};
+import React from 'react'
+import Navigation from './components/navigation/Navigation';
+
+function Page() {
+  return (
+    <div>
+        <Navigation/>
+    </div>
+  )
+}
 
 export default Page;
