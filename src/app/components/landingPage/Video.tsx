e0fd382560d4902f4e17fd823dc95a5a1fc05839
Ronjohnson Owuor <ronjohnsonowuor83@gmail.com>
2024-06-14 17:05:09 +0300

    solved and fixed some bugs but not yet done

--- src/app/components/landingPage/Video.tsx
+++ src/app/components/landingPage/Video.tsx
@@ -7,7 +7,9 @@ function Video() {
             <h3 className='text-[30px] font-bold'>Your <span className='bg-gradient-to-r from-primary via-error to-accent bg-clip-text text-transparent '>all-in-one</span> go to study ai</h3>
             <span className='text-gray text-center'>prepare for your exams effortlessly</span>
         </div>
-        <div className='w-[60%] h-[400px] bg-gray rounded-sm'></div>
+        <div className='w-[60%] h-[400px] bg-gray rounded-sm grid place-items-center'>
+          <p> video loading failed</p>
+        </div>
 
     </div>
   )
