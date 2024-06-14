bc32460075e902780c4f41065bd125075af17d4c
Ronjohnson Owuor <ronjohnsonowuor83@gmail.com>
2024-06-14 17:29:32 +0300

    fixing bugs in the source code

--- src/app/components/landingPage/Hero.tsx
+++ src/app/components/landingPage/Hero.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
 import { hero_picture } from '../container'
@@ -11,7 +12,7 @@ function Hero() {
             Achieve <span className='bg-gradient-to-r  via-error from-primary to-accent bg-clip-text text-transparent '>More</span> with Intelligent Study Solutions
             </h1>
             <span className='text-gray'>study with the best ai,get notes videos and more </span>
-            <button className='flex items-center justify-center w-[150px] h-[40px] rounded-md bg-accent hover:bg-primary my-4'>get started <RiArrowRightFill/> </button>
+            <button onClick={()=>window.location.href="/signup"}  className='flex items-center justify-center w-[150px] h-[40px] rounded-md bg-accent hover:bg-primary my-4'>get started <RiArrowRightFill/> </button>
         </div>
         <div className='w-[45%] mx-4 items-center justify-center'>
         <Image className='w-[70%] h-[70%] mx-4 my-4' src={hero_picture} alt='hero image'/>
