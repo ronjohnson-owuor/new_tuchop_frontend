3febcfeae51f8c534e6c6b601540ec5437e57593
Ronjohnson Owuor <ronjohnsonowuor83@gmail.com>
2024-06-16 13:30:13 +0300

    added the video to my website

--- src/app/components/landingPage/Video.tsx
+++ src/app/components/landingPage/Video.tsx
@@ -5,11 +5,11 @@ function Video() {
     <div className='w-full h-[600px] grid place-items-center mt-20'>
         <div className='text-center '>
             <h3 className='mx-[10%] text-center sm:mx-4 text-[30px] font-bold'>Your <span className='bg-gradient-to-r from-primary via-error to-accent bg-clip-text text-transparent '>all-in-one</span> go to study ai</h3>
-            <span className='text-gray text-center'>prepare for your exams effortlessly</span>
+            <span className='text-gray text-center'>prepare for your exams effortlessly,watch our website overview</span>
         </div>
-        <div className='w-[95%] sm:w-[60%] h-[400px] bg-gray rounded-sm grid place-items-center'>
-          <p> video loading failed</p>
-        </div>
+        <iframe src='https://www.youtube.com/embed/5jLLw-WiHg0?si=4jnCrYEZZLeSLqBg'  className='w-[95%] sm:w-[60%] h-[400px] bg-gray rounded-sm grid place-items-center'>
+          
+        </iframe>
 
     </div>
   )
